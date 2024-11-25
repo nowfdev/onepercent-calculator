e17bf573298af4728e6a23780b1f43de53ad65ac
nowfdev <nowf.dev@gmail.com>
2024-11-25 17:17:27 +0700

    Create UI and CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useReducer } from "react";
 
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentInput: `${state.currentInput || ""}${payload.digit}`,
+      };
+    default:
+  }
+}
 function App() {
+  const [{ currentInput, previousInput, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-input">
+          {previousInput} {operation}
+        </div>
+        <div className="current-input">{currentInput}</div>
+      </div>
+      <button>AC</button>
+      <button>+/-</button>
+      <button>%</button>
+      <button>/</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>X</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+      <button className="span-two">0</button>
+      <button>,</button>
+      <button>=</button>
     </div>
   );
 }
